31f347ad4fe2df20715cf65e12b8ef0fa7d46173
Gabriele Mandalari <mandalari.gabriele@gmail.com>
2014-03-18 17:54:19 +0100

    first commit

---
+++ FacebookLoginTest/src/main/java/com/facebooklogintest/MainActivity.java
@@ -0,0 +1,180 @@
+package com.facebooklogintest;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class MainActivity  extends FragmentActivity {
+
+    private LoginFragment mainFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment  = new LoginFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, mainFragment).commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (LoginFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public  class LoginFragment extends Fragment {
+
+        private UiLifecycleHelper uiHelper;
+        private static final String TAG = "MainFragment";
+
+
+        public LoginFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            try {
+                PackageInfo info = getPackageManager().getPackageInfo(
+                        "com.facebooklogintest",
+                        PackageManager.GET_SIGNATURES);
+                for (Signature signature : info.signatures) {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+
+            } catch (NoSuchAlgorithmException e) {
+
+            }
+            uiHelper = new UiLifecycleHelper(getActivity(), callback);
+            uiHelper.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+            LoginButton authButton = (LoginButton) rootView.findViewById(R.id.authButton);
+            authButton.setReadPermissions(Arrays.asList( "basic_info"));
+            authButton.setFragment(this);
+            return rootView;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            // For scenarios where the main activity is launched and user
+            // session is not null, the session state change notification
+            // may not be triggered. Trigger it if it's open/closed.
+            Session session = Session.getActiveSession();
+            if (session != null &&
+                    (session.isOpened() || session.isClosed()) ) {
+                onSessionStateChange(session, session.getState(), null);
+            }
+            uiHelper.onResume();
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            uiHelper.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            uiHelper.onPause();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            uiHelper.onDestroy();
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            uiHelper.onSaveInstanceState(outState);
+        }
+
+        private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+            if (state.isOpened()) {
+                Log.i(TAG, "Logged in...");
+                // Request user data and show the results
+                Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+                    @Override
+                    public void onCompleted(GraphUser user, Response response) {
+                        if (user != null) {
+                            String name = user.getName();
+                            Log.i(TAG, name);
+                        }
+                    }
+                });
+            } else if (state.isClosed()) {
+                Log.i(TAG, "Logged out...");
+            }
+        }
+
+        private Session.StatusCallback callback = new Session.StatusCallback() {
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+        };
+    }
+
+}
